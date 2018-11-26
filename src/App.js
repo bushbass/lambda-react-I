4c5cb2093d1f48064fbea741909c3cc0501875a3
Alex Nielsen <alex.nielsen@gmail.com>
2018-11-26 09:11:21 -0500

    testing codesandbox integration
    simple text changes just so I can figure out how this works with git

--- src/App.js
+++ src/App.js
@@ -1,64 +1,66 @@
-import React from 'react';
-import TodoList from './components/TodoComponents/TodoList'
-import TodoForm from './components/TodoComponents/TodoForm'
-
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
 
 class App extends React.Component {
-  
-  
   state = {
-    todoArray: [
-      {task:'laundry',
-        id: Date.now(),
-        completed:false},
-    ],
-    currentTodo: ''
-  }
-  
-  handleInputChange = (event)=> this.setState({currentTodo:event.target.value})
-  
+    todoArray: [{ task: "laundry", id: Date.now(), completed: false }],
+    currentTodo: ""
+  };
+
+  handleInputChange = event =>
+    this.setState({ currentTodo: event.target.value });
+
   handleSubmit = event => {
-    event.preventDefault()
-    const newEventObject= {
+    event.preventDefault();
+    const newEventObject = {
       completed: false,
       id: Date.now(),
       task: this.state.currentTodo
-    }
-      this.setState({todoArray: [...this.state.todoArray, newEventObject],
-      currentTodo: ''
-  })}
-  
+    };
+    this.setState({
+      todoArray: [...this.state.todoArray, newEventObject],
+      currentTodo: ""
+    });
+  };
+
   handleItemClick = id => {
-    this.setState({todoArray:[...this.state.todoArray].map(
-        todo => {
-          if(todo.id === id){
-            todo.completed = !todo.completed
-            return todo
-          } else {
-            return todo
+    this.setState({
+      todoArray: [...this.state.todoArray].map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+          return todo;
+        } else {
+          return todo;
         }
-    })
-  })}
-    
+      })
+    });
+  };
+
   handleClearCompleted = () => {
-    this.setState({todoArray:[...this.state.todoArray].filter(todo =>  todo.completed !== true )})
-  }
-    
+    this.setState({
+      todoArray: [...this.state.todoArray].filter(
+        todo => todo.completed !== true
+      )
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to Alex's Todo App!</h2>
-        
-        <TodoList 
+
+        <TodoList
           handleItemClick={this.handleItemClick}
-          todoArray={this.state.todoArray}/>
-        
-        <TodoForm 
-          currentTodo={this.state.currentTodo} 
+          todoArray={this.state.todoArray}
+        />
+
+        <TodoForm
+          currentTodo={this.state.currentTodo}
           handleInputChange={this.handleInputChange}
           handleSubmit={this.handleSubmit}
           handleClearCompleted={this.handleClearCompleted}
-          />
+        />
       </div>
     );
   }
