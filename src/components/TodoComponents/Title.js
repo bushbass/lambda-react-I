734f5fad3a40dc7cff24ae647ff3506f21c5e61e
bushbass <alex.nielsen@gmail.com>
2018-12-08 16:22:53 -0500

    changed name on front page

--- src/components/TodoComponents/Title.js
+++ src/components/TodoComponents/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
   return (
     <div>
-      <h2>Welcome to the to do app for Alex!</h2>
+      <h2>Welcome to the to do app for Alex Nielsen!</h2>
 
       <li>Add a to do</li>
       <li>Click a to do to mark it complete</li>
